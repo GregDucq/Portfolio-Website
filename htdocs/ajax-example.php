d41896d509f376505ee6ec11e783c5c9d08f8f50
Greg Ducq <greg.ducq@coolcadelectronics.com>
2021-06-10 16:27:09 -0400

    Added basic splite database interaction to php-test.html and ajax-example.php

--- htdocs/ajax-example.php
+++ htdocs/ajax-example.php
@@ -1,3 +1,12 @@
 <?php
-   echo "Hello World!";
+	$name = $_GET["name"];
+	$id = $_GET["id"];
+	$query = 'INSERT INTO basic_id (name, id) VALUES(\'' . $name . '\', \'' . $id . '\')';
+	
+	$db = new SQLite3('../sql/test.db');
+	//$db->query($query);
+	
+	$processed_text = "You entered: " . $name . ". Their ID is: " . $id;
+	
+	echo $processed_text
 ?>
