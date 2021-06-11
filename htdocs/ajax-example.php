5ed1531953bedfde8799924b12fb3d904cfcf26b
Greg Ducq <greg.ducq@coolcadelectronics.com>
2021-06-11 14:40:01 -0400

    Added basic controls for clearing basic_id database and to display all values stored in the database. Also added styling rules to basic ajax program.

--- htdocs/ajax-example.php
+++ htdocs/ajax-example.php
@@ -1,12 +1,42 @@
 <?php
-	$name = $_GET["name"];
-	$id = $_GET["id"];
-	$query = 'INSERT INTO basic_id (name, id) VALUES(\'' . $name . '\', \'' . $id . '\')';
+	$event = $_GET["event"];
 	
-	$db = new SQLite3('../sql/test.db');
-	//$db->query($query);
+	if($event == "add"){
+		$name = $_GET["name"];
+		$id = $_GET["id"];
+		$query = 'INSERT INTO basic_id (name, id) VALUES(\'' . $name . '\', \'' . $id . '\');';
+		
+		$db = new SQLite3('../sql/test.db');
+		$db->query($query);
+		
+		$processed_text = "You entered: " . $name . ". Their ID is: " . $id;
+		
+		echo $processed_text;
+	}
+		
+	elseif($event == "display"){
+		$query = 'SELECT * from basic_id';
+		$db = new SQLite3('../sql/test.db');
+		
+		$res = $db->query($query);
+		$processed_text = "Values entered into database:\n";
+		
+		while($row = $res->fetchArray()){
+			$processed_text = $row[0] . " | " . $row[1] . "\n"; 
+		}
+		
+		echo $processed_text;
+	}
 	
-	$processed_text = "You entered: " . $name . ". Their ID is: " . $id;
+	elseif($event == "clear"){
+		$query = 'DELETE FROM basic_id;';
+		
+		$db = new SQLite3('../sql/test.db');
+		$db->query($query);
+		echo "ID database successfully cleared.";
+	}
 	
-	echo $processed_text
+	else{
+		echo "Event error.";
+	}
 ?>
