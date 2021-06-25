3143864078e3a2a852e197ba2d2a781947bd69b3
Greg Ducq <greg.ducq@coolcadelectronics.com>
2021-06-24 22:14:39 -0400

    Added controls to allow user to select specific entries to display or delete.

--- htdocs/php/student-info-access.php
+++ htdocs/php/student-info-access.php
@@ -1,5 +1,5 @@
 <?php
-	error_reporting (E_ERROR); // Restrict error reporting to errors only to prevent failed SQL queries from being included in output.
+	//error_reporting (E_ERROR); // Restrict error reporting to errors only to prevent failed SQL queries from being included in output.
 	
 	$postdata = file_get_contents("php://input");
 	$request = json_decode($postdata);
@@ -26,10 +26,63 @@
 		else{
 			echo "Failed to enter student";
 		}
+		
+		if(empty($request->firstName)){
+			echo "NO FIRST NAME GIVEN!";
+		}
+		
+		else{
+			echo "FIRST NAME FOUND";
+		}
 	}
 	
 	elseif($request->command == 1){
-		$query = "select * from studentInfo;";
+		$conditions = "";
+		
+		// Check for specific values to pull up.
+		if(!empty($request->studentID)){
+			$conditions .= (" studentID='" . $request->studentID . "'");
+		}
+		
+		if(!empty($request->firstName)){
+			$conditions .= (" firstName='" . $request->firstName . "'");
+		}
+		
+		if(!empty($request->lastName)){
+			$conditions .= (" lastName='" . $request->lastName . "'");
+		}
+		
+		if(!empty($request->address)){
+			$conditions .= (" address='" . $request->address . "'");
+		}
+		
+		if(!empty($request->city)){
+			$conditions .= (" city='" . $request->city . "'");
+		}
+		
+		if(!empty($request->state)){
+			$conditions .= (" state='" . $request->state . "'");
+		}
+		
+		if(!empty($request->country)){
+			$conditions .= (" country='" . $request->country . "'");
+		}
+		
+		if(!empty($request->phone)){
+			$conditions .= (" phone='" . $request->phone . "'");
+		}
+		
+		if(!empty($request->email)){
+			$conditions .= (" email='" . $request->email . "'");
+		}
+		
+		// Construct query based on what conditions are given
+		$query = "select * from studentInfo";
+		if(!empty($conditions)){
+			$query .= " where" . $conditions;
+		}
+		$query .= ";";
+		
 		$res = $db->query($query);
 		$json_result = array();
 		while($row = $res->fetchArray(SQLITE3_ASSOC)){
@@ -38,12 +91,58 @@
 		}
 		
 		echo json_encode($json_result);
+		//echo $query;
 	}
 	
 	elseif($request->command == 2){
-		$query = 'delete from studentInfo where studentID=\'' . $request->studentID . '\';';
+				$conditions = "";
+		
+		// Check for specific values to pull up.
+		if(!empty($request->studentID)){
+			$conditions .= (" studentID='" . $request->studentID . "'");
+		}
+		
+		if(!empty($request->firstName)){
+			$conditions .= (" firstName='" . $request->firstName . "'");
+		}
+		
+		if(!empty($request->lastName)){
+			$conditions .= (" lastName='" . $request->lastName . "'");
+		}
+		
+		if(!empty($request->address)){
+			$conditions .= (" address='" . $request->address . "'");
+		}
+		
+		if(!empty($request->city)){
+			$conditions .= (" city='" . $request->city . "'");
+		}
+		
+		if(!empty($request->state)){
+			$conditions .= (" state='" . $request->state . "'");
+		}
+		
+		if(!empty($request->country)){
+			$conditions .= (" country='" . $request->country . "'");
+		}
+		
+		if(!empty($request->phone)){
+			$conditions .= (" phone='" . $request->phone . "'");
+		}
+		
+		if(!empty($request->email)){
+			$conditions .= (" email='" . $request->email . "'");
+		}
+		
+		// Construct query based on what conditions are given
+		$query = "delete from studentInfo";
+		if(!empty($conditions)){
+			$query .= " where" . $conditions;
+		}
+		$query .= ";";
+		
 		$db->query($query);
 		
-		echo "Student info deleted with query: " . $query;
+		echo "Student info from student records.";
 	}
 ?>
