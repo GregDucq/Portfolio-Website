3e3f00fe2b1f0792d6b2c2839df76e0c3cbc6928
Greg Ducq <greg.ducq@coolcadelectronics.com>
2021-07-07 23:26:19 -0400

    Added course status to SQL queries and corrected how it updates gradeVal when grade is updated.

--- htdocs/php/student-course-info-access.php
+++ htdocs/php/student-course-info-access.php
@@ -1,18 +1,37 @@
 <?php
 	error_reporting (E_ERROR); // Restrict error reporting to errors only to prevent failed SQL queries from being included in output.
 	
+	$gradeVals = array(
+    "A+" => 4,
+    "A" => 4,
+	"A-" => 3.7,
+	"B+" => 3.3,
+	"B" => 3,
+	"B-" => 2.7,
+	"C+" => 2.3,
+	"C" => 2,
+	"C-" => 1.7,
+	"D+" => 1.3,
+	"D" => 1,
+	"D-" => 0.7,
+	"F" => 0);
+	
 	$postdata = file_get_contents("php://input");
 	$request = json_decode($postdata);
 	$db = new SQLite3('../../sqlite3/dbs/college-records/college-records.db');
 	$db->exec("PRAGMA foreign_keys = ON;"); // Needed to enforce foreign key links
 	
-	if($request->command == 0){	
-		$query = 'INSERT INTO studentCourseInfo (studentID, courseID, grade) VALUES('. 
+	if($request->command == 0){
+		$query = 'INSERT INTO studentCourseInfo (studentID, courseID, grade, gradeVal, courseStatus) VALUES('. 
 			'\'' . $request->studentID . '\',' .
 			'\'' . $request->courseID . '\',' .
-			'\'' . $request->grade . 
+			'\'' . $request->grade . '\',' .
+			'\'' . $gradeVals[$request->grade] . '\',' .
+			'\'' . $request->courseStatus .
 			'\');';
-		
+
+		echo $query;
+
 		$res = $db->exec($query);
 		
 		if($res){
@@ -46,6 +65,14 @@
 			}
 			
 			$conditions .= (" grade='" . $request->grade . "'");
+		}
+		
+		if(!empty($request->courseStatus)){
+			if(!empty($conditions)){
+				$conditions .= " and";
+			}
+			
+			$conditions .= (" courseStatus='" . $request->courseStatus . "'");
 		}
 		
 		// Construct query based on what conditions are given
@@ -89,6 +116,14 @@
 			$conditions .= (" grade='" . $request->grade . "'");
 		}
 		
+		if(!empty($request->courseStatus)){
+			if(!empty($conditions)){
+				$conditions .= " and";
+			}
+			
+			$conditions .= (" courseStatus='" . $request->courseStatus . "'");
+		}
+		
 		// Construct query based on what conditions are given
 		$query = "delete from studentCourseInfo";
 		if(!empty($conditions)){
@@ -103,22 +138,21 @@
 	
 	elseif($request->command == 3){
 		$conditions = "";
-		
-		
-		if(!empty($request->courseID)){
-			if(!empty($conditions)){
-				$conditions .= ", ";
-			}
 			
-			$conditions .= (" courseID='" . $request->courseID . "'");
-		}
-		
 		if(!empty($request->grade)){
 			if(!empty($conditions)){
 				$conditions .= ", ";
 			}
 			
-			$conditions .= (" grade='" . $request->grade . "'");
+			$conditions .= (" grade='" . $request->grade . "', gradeVal=" . $gradeVals[$request->grade] . " ");
+		}
+		
+		if(!empty($request->courseStatus)){
+			if(!empty($conditions)){
+				$conditions .= ", ";
+			}
+			
+			$conditions .= (" courseStatus='" . $request->courseStatus . "'");
 		}
 		
 		if(empty($conditions)){
@@ -126,7 +160,8 @@
 		}
 		
 		else{
-			$query = "update studentCourseInfo set" . $conditions . " where studentID='" . $request->studentID . "';";
+			$query = "update studentCourseInfo set" . $conditions . " where studentID='" . $request->studentID . "' and courseID= '" . $request->courseID . "';";
+			
 			$db->exec($query);
 			
 			if($db->changes()){
